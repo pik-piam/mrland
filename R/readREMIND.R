2da1ec69e2884883d347838ccd1204ee88f6e302
David Klein <dklein@pik-potsdam.de>
2020-07-14 14:15:55 +0200

    Added recent coupled REMIND-MAgPIE results (currently used to provide bioenergy demand and GHG price scenarios).

--- R/readREMIND.R
+++ R/readREMIND.R
@@ -21,7 +21,23 @@ readREMIND <- function(subtype) {
                  "REMIND_generic_r8473-trunk-C_Budg950-rem-5.mif",
                  "REMIND_generic_r8473-trunk-C_Budg1300-rem-5.mif",
                  "REMIND_generic_r8473-trunk-C_NDC-rem-5.mif",
-                 "REMIND_generic_r8473-trunk-C_NPi-rem-5.mif")
+                 "REMIND_generic_r8473-trunk-C_NPi-rem-5.mif",
+                 "REMIND_generic_C_SDP-NPi-rem-5.mif",
+                 "REMIND_generic_C_SDP-PkBudg900-rem-5.mif",
+                 "REMIND_generic_C_SDP-PkBudg1000-rem-5.mif",
+                 "REMIND_generic_C_SDP-PkBudg1100-rem-5.mif",
+                 "REMIND_generic_C_SSP1-NPi-rem-5.mif",     
+                 "REMIND_generic_C_SSP1-PkBudg900-rem-5.mif",
+                 "REMIND_generic_C_SSP1-PkBudg1100-rem-5.mif",
+                 "REMIND_generic_C_SSP1-PkBudg1300-rem-5.mif",
+                 "REMIND_generic_C_SSP2-NPi-rem-5.mif",
+                 "REMIND_generic_C_SSP2-PkBudg900-rem-5.mif",
+                 "REMIND_generic_C_SSP2-PkBudg1100-rem-5.mif",
+                 "REMIND_generic_C_SSP2-PkBudg1300-rem-5.mif",
+                 "REMIND_generic_C_SSP5-NPi-rem-5.mif",
+                 "REMIND_generic_C_SSP5-PkBudg900-rem-5.mif",
+                 "REMIND_generic_C_SSP5-PkBudg1100-rem-5.mif",
+                 "REMIND_generic_C_SSP5-PkBudg1300-rem-5.mif")
   
   x <- NULL
   for(f in file_list) {
@@ -32,9 +48,9 @@ readREMIND <- function(subtype) {
   x <- collapseNames(x)
   
   # shorten names of the REMIND scenarios
-  getNames(x) <- gsub("r8473-trunk-C_", "SSP2-",getNames(x))
+  getNames(x) <- gsub("r8473-trunk-C_", "R2M41-SSP2-",getNames(x))
+  getNames(x) <- gsub("C_(SDP|SSP)", "R21M42-\\1",getNames(x))
   getNames(x) <- gsub("-rem-5","",getNames(x))
-  getNames(x) <- paste0("R2M41-",getNames(x))
 
   return(x)
 
