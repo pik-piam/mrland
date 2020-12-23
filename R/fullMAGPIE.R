5686be96c25c7b05fbe4b7b6b7240dd13be6242a
abhimishr <mishra.abhijeet28@gmail.com>
2020-12-23 01:19:13 +0100

    ouput file format change

--- R/fullMAGPIE.R
+++ R/fullMAGPIE.R
@@ -110,7 +110,7 @@ fullMAGPIE <- function(rev=0.1) {
     calcOutput("TimberHarvestCost",file = "f32_harvestingcost.cs4")
     
     # 35 natural vegetation
-    calcOutput("ForestFireShare",years = 2000, round=7, file="f35_forest_fire_share.csv")
+    calcOutput("ForestFireShare",years = 2000, round=7, file="f35_forest_fire_share.cs3")
 
     #38 factor costs
     calcOutput("Yield", cut=0.95, years=1995, round=2, file="f38_region_yield.csv")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MadRaT land data package
 
-R package **mrland**, version **0.5.12**
+R package **mrland**, version **0.5.13**
 
 [![Travis build status](https://travis-ci.com/pik-piam/mrland.svg?branch=master)](https://travis-ci.com/pik-piam/mrland) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3822083.svg)](https://doi.org/10.5281/zenodo.3822083) 
 
