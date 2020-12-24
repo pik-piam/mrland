baca3d845c1fadaa059051cba2337abea62126a0
abhimishr <mishra.abhijeet28@gmail.com>
2020-12-24 16:34:28 +0100

    growing stock target data added

--- R/fullMAGPIE.R
+++ R/fullMAGPIE.R
@@ -56,7 +56,8 @@ fullMAGPIE <- function(rev=0.1) {
     calcOutput("CalibrationDummy", round=0, file="f14_yld_calib.csv")
     calcOutput("PastureYield",round=3,file="f14_pasture_yields_hist.csv")
     calcOutput("Yield", cut=0.95, years=mag_years_past, round=2, file="f14_region_yields.cs3")
-    
+    calcOutput("GrowingStockPlantations",aggregate=TRUE,round=0,file="f14_gs_target.cs3")
+
     # 15 food
     calcOutput("BodyHeight",aggregate = FALSE,years=mag_years_past, round=2, file="f15_bodyheight_historical.cs3")
     calcOutput("RegressionParameters",aggregate = FALSE, round=3, file="f15_schofield_parameters.cs3", regression="Schofield")
