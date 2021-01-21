3158b457333b6c3aac216e486b8f6bb1f2e599f3
abhimishr <mishra.abhijeet28@gmail.com>
2021-01-21 22:30:44 +0100

    Additional scenario added for future plantaiton share dev in demand

--- R/calcPlantationContribution.R
+++ R/calcPlantationContribution.R
@@ -21,7 +21,7 @@ calcPlantationContribution <- function(){
   out <- setYears(out,"y1995")
 
   ## Create Scenarios
-  scen <- c("constant","h5s5l5","h5s2l2","h5s2l1","h5s1l1","h5s1l05")
+  scen <- c("constant","h5s5l5","h5s2l2","h5s2l1","h5s1l1","h5s1l05","h2s1l05")
   
   ## Years
   year <- paste0("y",seq(2000,2250,5))
@@ -33,33 +33,41 @@ calcPlantationContribution <- function(){
   out <- time_interpolate(dataset = out,interpolated_year = year,integrate_interpolated_years = TRUE,extrapolation_type = "constant")
   out <- add_dimension(x = out,dim = 3.2,nm = scen)
   
-  out_scen <- out[,,"constant",invert=TRUE]
+  out_scen <- out[,,"constant",invert = TRUE]
   
   mods <- str_extract_all(scen, "\\d+")
   names(mods) <- scen
   
-  for (i in getNames(out_scen,dim="new")) {
+  for (i in getNames(out_scen,dim = "new")) {
     for (j in 2:length(year)) {
       present <- getYears(out_scen)[j]
-      past    <- getYears(out_scen)[j-1]
-      if(present %in% hist) out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * (1+as.numeric(mods[[i]][1])/100)
-      if(present %in% short) out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * (1+as.numeric(mods[[i]][2])/100)
-      if(i!="h5s1l05" && present %in% long) out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * (1+as.numeric(mods[[i]][3])/100)
-      if(i=="h5s1l05" && present %in% long) out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * (1.005)
+      past    <- getYears(out_scen)[j - 1]
+      
+      scen_pattern <- mods[[i]]
+      multiplier <- c((1 + as.numeric(mods[[i]])/100))
+
+      if (length(grep(pattern = "0",x = scen_pattern,value = TRUE)) > 0) {
+        pos <- match(grep(pattern = "0",x = scen_pattern,value = TRUE),scen_pattern)
+        multiplier[pos] <- (1 + as.numeric(grep(pattern = "0",x = scen_pattern,value = TRUE))/1000)
+      }
+      
+      if (present %in% hist)  out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * multiplier[1]
+      if (present %in% short) out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * multiplier[2]
+      if (present %in% long)  out_scen[,present,i] <- setYears(out_scen[,past,i],NULL) * multiplier[3]
     }
   }
   out[,,getNames(out_scen)] <- out_scen
-  out[,length(year)+1,] <- out[,length(year),]
-  out[out>1] <- 1
+  out[,length(year) + 1,] <- out[,length(year),]
+  out[out > 1] <- 1
   out <- round(out,3)
 
   ## Weight
   weight <- collapseNames(calcOutput("TimberDemand",aggregate = FALSE)[,"y1995","production"])[,,c("Roundwood")]
   
-  return(list(x=out,
-              weight=weight,
-              min=0,
-              unit="percent",
-              description="Calculates the share of roundwood production coming from timber plantations"))
+  return(list(x = out,
+              weight = weight,
+              min = 0,
+              unit = "percent",
+              description = "Calculates the share of roundwood production coming from timber plantations"))
   
 }
