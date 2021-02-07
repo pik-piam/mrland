2c7137b1d61e0f86880f58486c1f6e20a67e1773
abhimishr <mishra.abhijeet28@gmail.com>
2021-02-07 19:23:59 +0100

    regional product specific demand added

--- R/fullMAGPIE.R
+++ R/fullMAGPIE.R
@@ -191,4 +191,5 @@ fullMAGPIE <- function(rev=0.1) {
     
     #73 timber -- Always needed on iso country level so no need to aggregate
     calcOutput("EndUseTimber", aggregate = FALSE, round=4, file="f73_prod_specific_timber.csv")
+    calcOutput("EndUseTimber", aggregate = TRUE, round=2, file="f73_regional_timber_demand.csv")
 }
