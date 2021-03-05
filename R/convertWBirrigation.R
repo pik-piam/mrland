694cd0a8ced27c44e8488be3da84f4d26e2b2984
FeBeier <felicitas.beier@pik-potsdam.de>
2021-03-05 11:51:06 +0100

    included new FAO_online in calcIrrigationInvCosts

--- R/convertWBirrigation.R
+++ R/convertWBirrigation.R
@@ -1,16 +1,16 @@
-#' Convert WBirrigation data
+#' @title convertWBirrigation 
 #' 
-#' Convert WorldBank-irrigation data on ISO country level.
-#' 
+#' @description Convert WorldBank-irrigation data on ISO country level.
 #' 
 #' @param x MAgPIE object containing WBirrigation data country-region
 #' resolution
-#' @return WBirrigation data as MAgPIE object aggregated to country level
-#' @author Lavinia Baumstark
-#' @examples
 #' 
-#' \dontrun{ a <- convertWBirrigation(x)
-#' }
+#' @return WBirrigation data as MAgPIE object aggregated to country level
+#' 
+#' @author Lavinia Baumstark
+#' 
+#' @examples
+#' \dontrun{ a <- convertWBirrigation(x) }
 #' 
 convertWBirrigation <- function(x) {
   x <- x[!(getRegions(x) %in% c("AFR","ESA")),,]
