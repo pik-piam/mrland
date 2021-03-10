fe827dd4eb6dd7afa703ebe55a7a1c56c55b7971
FeBeier <felicitas.beier@pik-potsdam.de>
2021-03-10 23:17:07 +0100

    updated calcEFch4AWMS and calcEFch4Rice to readFAO_online

--- R/calcEFch4AWMS.R
+++ R/calcEFch4AWMS.R
@@ -12,42 +12,41 @@
 #' calcOutput("EFch4AWMS")
 #' }
 #' 
-#' @importFrom magclass getSets clean_magpie
+#' @importFrom magclass getSets clean_magpie collapseNames
 #' @importFrom stats median
 
 calcEFch4AWMS<-function(){
   
-  emis<-readSource("FAO","EmisAgManureManag")
-  ch4<-emis[,,c("Emissions_(CH4)_(Manure_management)_(Gigagrams)","Manure_(N_content)_(Manure_management)_(Kg)")]
+  emis   <- readSource("FAO_online","EmisAgManureManag")
+  ch4    <- emis[,,c("Emissions_(CH4)_(Manure_management)_(gigagrams)","Manure_treated_(N_content)_(kg)")]
 
-  map <- toolMappingFile(type = "sectoral",name="FAOitems_animals_ch4.csv",readcsv=TRUE, where = "mrland")
+  map    <- toolMappingFile(type = "sectoral", name="FAOitems_animals_ch4.csv", readcsv=TRUE, where = "mrland")
   ch4_ag <- toolAggregate(x = ch4,dim = 3.1,rel = map,from = "fao",to="magpie",partrel = TRUE, verbosity = 2)
-  ef <-  collapseNames((ch4_ag[,,"Emissions_(CH4)_(Manure_management)_(Gigagrams)"]/1000)
-         /(ch4_ag[,,"Manure_(N_content)_(Manure_management)_(Kg)"]/1000000000))
+  ef     <- collapseNames((ch4_ag[,,"Emissions_(CH4)_(Manure_management)_(gigagrams)"]/1000) / (ch4_ag[,,"Manure_treated_(N_content)_(kg)"]/1000000000))
   
   # countries without values get the world average
   for (i in getNames(ef,dim=1)){
-    tmp<-ef[,,i]
-    tmp[is.nan(tmp)]<-median(ef[,,i],na.rm = T)
-    tmp[tmp==0]<-median(ef[,,i],na.rm = T)
-    tmp[tmp==Inf]<-median(ef[,,i],na.rm = T)
-    ef[,,i]<-tmp
+    tmp              <- ef[,,i]
+    tmp[is.nan(tmp)] <- median(ef[,,i],na.rm = T)
+    tmp[tmp==0]      <- median(ef[,,i],na.rm = T)
+    tmp[tmp==Inf]    <- median(ef[,,i],na.rm = T)
+    ef[,,i]          <- tmp
   }
   
-  weight=collapseNames(ch4_ag[,,"Manure_(N_content)_(Manure_management)_(Kg)"]/1000000000)
+  weight <- collapseNames(ch4_ag[,,"Manure_treated_(N_content)_(kg)"]/1000000000)
   
   # extend for whole period
-  time<-findset("time")
-  ef<-time_interpolate(dataset = ef,
+  time  <- findset("time")
+  ef    <- time_interpolate(dataset = ef,
                        interpolated_year = time,
                        integrate_interpolated_years = FALSE,
                        extrapolation_type = "constant")
-  ef<-clean_magpie(ef)
-  weight<-time_interpolate(dataset = weight,
+  ef     <- clean_magpie(ef)
+  weight <- time_interpolate(dataset = weight,
                        interpolated_year = time,
                        integrate_interpolated_years = FALSE,
                        extrapolation_type = "constant")
-  weight<-clean_magpie(weight)
+  weight <- clean_magpie(weight)
   
   
   return(list(x=ef,
