07e135f6c5ef360f011c1a720a289ab9b74a5b8a
florianh <humpenoeder@pik-potsdam.de>
2021-05-15 19:20:00 +0200

    added GHG prices and Bioenergy demand from Strefler et al 2021

---
+++ R/convertStrefler2021.R
@@ -0,0 +1,22 @@
+convertStrefler2021 <- function(x,subtype) {
+  
+  # remove global dimension
+  x <- x["GLO",,,invert=TRUE]
+  
+  map <- toolMappingFile("regional","regionmappingH12.csv")
+  
+  if(grepl("intensive",subtype)) {
+    # No weight for disaggregation because it's prices
+    y <- toolAggregate(x,map) 
+    
+  } else if(grepl("extensive",subtype)) {
+    # Use population of 2010 as weight for disaggregation
+    pop <- calcOutput("Population",aggregate=FALSE)
+    y <- toolAggregate(x,map,weight=pop[,2010,1])
+    
+  } else {
+    stop("Unknown subtype ",subtype)
+  }
+  
+  return(y)
+}
