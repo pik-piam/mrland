6f0b6527f144ed8d6f137babcf7f09069a678af5
Debbora Leip <leip@pik-potsdam.de>
2022-02-25 11:05:53 +0100

    fixed warning in readGTAP

--- R/readGTAP.R
+++ R/readGTAP.R
@@ -33,11 +33,11 @@
 #' @return GTAP data as a MAgPie-Object
 #' @author Stephen Wirth, Xiaoxi Wang
 #' @examples
-#'   \dontrun{
-#'     a <- readSource("GTAP7", "VIWS")
-#'            }
+#' \dontrun{
+#' a <- readSource("GTAP7", "VIWS")
+#' }
 #' @importFrom reshape2 acast
-#' @importFrom magclass as.magpie getYears unwrap getYears<- getRegions<-
+#' @importFrom magclass as.magpie getYears unwrap getYears<- getCells<-
 #' @importFrom utils read.csv
 readGTAP <- function(subtype = NULL) {
   files <- c(
@@ -60,11 +60,11 @@ readGTAP <- function(subtype = NULL) {
     GTAP8_VOM  = "BaseView.csv",
     GTAP8_VOA  = "BaseView.csv"
   )
-  
-    file <- toolSubtypeSelect(subtype, files)
-    
-    path <-
-      paste0(tolower(gsub("(?<=\\d)\\w{1,}", "", subtype, perl = T)), "/")
+
+  file <- toolSubtypeSelect(subtype, files)
+
+  path <-
+    paste0(tolower(gsub("(?<=\\d)\\w{1,}", "", subtype, perl = TRUE)), "/")
   if (grepl("BaseData.csv", file)) {
     x <-
       read.csv(
@@ -72,59 +72,60 @@ readGTAP <- function(subtype = NULL) {
         header = FALSE,
         stringsAsFactors = FALSE,
         skip = 3
-      )} else if (grepl("BaseView.csv", file)){
-        x <-
-          read.csv(
-            paste0(path, file),
-            header = FALSE,
-            stringsAsFactors = FALSE,
-            skip = 0)}
-    
-    all_start <- which(grepl("!Header:", x$V1))
-    dims <- x$V2[which(grepl("dimensions", x$V2))]
-    gtapval <- sub("[A-Z]{4}\\d_", "", subtype, perl = T)
-    start_row <- which(grepl(paste("!Header:",gtapval,sep=" "), x$V1))
-    if (start_row == max(all_start)) {
-      endrow <- nrow(x)
-    }else{
-      count <- match(start_row, all_start)
-      endrow <- all_start[count + 1] - 1
-    }
-    x <- x[c((start_row + 1):endrow), ]
-    if (x[nrow(x), ncol(x)] == "") {
-      x <- x[-nrow(x), ]
-    }
-    if (length(grep("REG", x[1, ])) > 1) {
-      x[1, grep("REG", x[1, ])][1] <- "Regions"
-    }
-    else
-      x[1, grep("REG", x[1, ])] <- "Regions"
-    colnames(x) <- x[1, ]
-    x <- x[-1, ]
-    if (grep("Regions", colnames(x)) != 1)
-    {
-      x[, c(1, grep("Regions", colnames(x)))] <-
-        x[, c(grep("Regions", colnames(x)), 1)]
-      colnames(x)[c(1, grep("Regions", colnames(x)))] <-
-        colnames(x)[c(grep("Regions", colnames(x)), 1)]
-    }
-    x$Value <- as.numeric(x$Value)
-    out <- as.magpie(x, spatial = 1, tidy = TRUE)
-    if("REG" %in% colnames(x)){
+      )
+  } else if (grepl("BaseView.csv", file)) {
+    x <-
+      read.csv(
+        paste0(path, file),
+        header = FALSE,
+        stringsAsFactors = FALSE,
+        skip = 0
+      )
+  }
+
+  allStart <- which(grepl("!Header:", x$V1))
+  gtapval <- sub("[A-Z]{4}\\d_", "", subtype, perl = TRUE)
+  startRow <- which(grepl(paste("!Header:", gtapval, sep = " "), x$V1))
+  if (startRow == max(allStart)) {
+    endrow <- nrow(x)
+  } else {
+    count <- match(startRow, allStart)
+    endrow <- allStart[count + 1] - 1
+  }
+  x <- x[c((startRow + 1):endrow), ]
+  if (x[nrow(x), ncol(x)] == "") {
+    x <- x[-nrow(x), ]
+  }
+  if (length(grep("REG", x[1, ])) > 1) {
+    x[1, grep("REG", x[1, ])][1] <- "Regions"
+  } else {
+    x[1, grep("REG", x[1, ])] <- "Regions"
+  }
+  colnames(x) <- x[1, ]
+  x <- x[-1, ]
+  if (grep("Regions", colnames(x)) != 1) {
+    x[, c(1, grep("Regions", colnames(x)))] <-
+      x[, c(grep("Regions", colnames(x)), 1)]
+    colnames(x)[c(1, grep("Regions", colnames(x)))] <-
+      colnames(x)[c(grep("Regions", colnames(x)), 1)]
+  }
+  x$Value <- as.numeric(x$Value)
+  out <- as.magpie(x, spatial = 1, tidy = TRUE)
+  if ("REG" %in% colnames(x)) {
     x <- unwrap(out)
     x <- aperm(x, c(1, 2, 4, 3))
     x <- as.magpie(x, spatial = 1, temporal = 2)
-    }
-    else {x <- out}
-    #is GTAP released every 3 years?
-    if (grepl("GTAP7", subtype)) {
-      year <- "y2004"
-    }
-    else if (grepl("GTAP8", subtype)) {
-      year <- "y2007"
-    }
-    
-    getYears(x) <- year
-    getRegions(x) <- toupper(getRegions(x))
-    return(x)
+  } else {
+    x <- out
+  }
+  # is GTAP released every 3 years?
+  if (grepl("GTAP7", subtype)) {
+    year <- "y2004"
+  } else if (grepl("GTAP8", subtype)) {
+    year <- "y2007"
+  }
+
+  getYears(x) <- year
+  getCells(x) <- toupper(getCells(x))
+  return(x)
 }
