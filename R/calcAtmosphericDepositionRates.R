1a5553bab4c83ccc81f10fe0a4518e7534872e74
Marcos Alves <mppalves@gmail.com>
2022-03-02 08:45:01 +0100

    bug fix dep_scen50 calcAtmosphericDepositionRates

--- R/calcAtmosphericDepositionRates.R
+++ R/calcAtmosphericDepositionRates.R
@@ -22,8 +22,8 @@ calcAtmosphericDepositionRates<-function(cellular=FALSE){
   luhdata<-calcOutput("LanduseInitialisation",cellular=cellular,aggregate=FALSE)
   dep <- toolHoldConstantBeyondEnd(dep)
   weight <- toolHoldConstantBeyondEnd(luhdata)
-  dep <- collapseNames(dep)
-  weight <- collapseNames(weight)
+  # dep <- collapseNames(dep)
+  # weight <- collapseNames(weight)
   dep <- dep*(weight>10^-7)
   
   out<-dep/weight
