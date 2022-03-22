c671cc4a76774b1df497fb8f56da1e3208df841f
Marcos Alves <mppalves@gmail.com>
2022-03-22 17:35:15 +0100

    Correcting livestock temporal distribution in Brasil

--- R/calcGrasslandBiomass.R
+++ R/calcGrasslandBiomass.R
@@ -21,13 +21,41 @@ calcGrasslandBiomass <- function() {
   grasslLand <- setNames(grasslLand, c("pastr", "range"))
   grasslShares <- setNames(grasslLand[, , "pastr"] / dimSums(grasslLand, dim = 3), "pastr")
   grasslShares <- add_columns(grasslShares, addnm = "range", dim = 3.1)
-  grasslShares[, , "range"] <- 1 - grasslShares[, , "pastr"]
   grasslShares[is.nan(grasslShares) | is.infinite(grasslShares)] <- 0
+  grasslShares[, , "range"] <- 1 - grasslShares[, , "pastr"]
 
   mapping <- toolGetMapping("CountryToCellMapping.rds", where = "mrcommons")
-
+  
   livestock <- setNames(toolCell2isoCell(readSource("GLW3")), "liv_numb")
+  
+  # I am working with the assumption that in most places, the proportional distribution of 
+  # livestock heads was kept the same between 1965 and 2010. One notable exception is Brasil, 
+  # which experienced a shift in its agricultural frontier towards the west with increases in 
+  # rangelands used for cattle production. A "fader" is used to correct the proportional share 
+  # of livestock being in rangelands and managed pastures, and avoid distortions on the amount 
+  # of grass biomass production assigned to each system.
+  
   livstSplit <- livestock * grasslShares
+  
+  # Fader calculation
+  
+  fader <- livstSplit
+  fader[,,] <- 1
+  start_pastr <- 1.2
+  end_pastr <- 1
+  start_range <- 0.8
+  end_range <- 1
+  range_values <- seq(start_range,end_range,(end_range-start_range)/(length(getItems(fader, dim = 2))-1))
+  pastr_values <- seq(start_pastr,end_pastr,(end_pastr-start_pastr)/(length(getItems(fader, dim = 2))-1))
+  for (i in 1:length(range_values)) {
+    fader["BRA",i,"range"] <-  range_values[i]
+  }
+  for (i in 1:length(pastr_values)) {
+    fader["BRA",i,"pastr"] <-  pastr_values[i]
+  }
+  livstSplit <-  livstSplit * fader
+  
+  
   livstSplit <- collapseNames(livstSplit)
   livstSplitCtry <- toolAggregate(livstSplit, rel = mapping, to = "iso", from = "celliso")
   livstShareCtry <- livstSplitCtry[, , "pastr"] / dimSums(livstSplitCtry, dim = 3)
