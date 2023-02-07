6311e5ef9e7eb09f4292bcaf90204d389fcb413c
caviddhen <david.chen@pik.potsdam.de>
2023-02-07 12:39:29 +0100

    update trade margin to include missing products and use FAOini price by default

--- R/calcTradeMargin.R
+++ R/calcTradeMargin.R
@@ -17,16 +17,15 @@
 #' @importFrom magpiesets findset
 #' @importFrom magclass is.magpie
 
-calcTradeMargin <- function(gtap_version = "GTAP7", bilateral = FALSE, producer_price = "FAO") { # nolint
+calcTradeMargin <- function(gtap_version = "GTAP7", bilateral = FALSE, producer_price = "FAOini") { # nolint
   stopifnot(gtap_version %in% c("GTAP7", "GTAP8"))
   viws <- calcOutput(type = "GTAPTrade", subtype = paste(gtap_version, "VIWS", sep = "_"),
                      bilateral = bilateral, aggregate = FALSE)
   vxwd <- calcOutput(type = "GTAPTrade", subtype = paste(gtap_version, "VXWD", sep = "_"),
                      bilateral = bilateral, aggregate = FALSE)
-   vtwr <- viws - vxwd # imports - exports
+   vtwr <- viws - vxwd # imports values at  W price - export value at W Price
 
   vtwr[vtwr < 0] <- 0
-
 
   vxmd <- calcOutput(type = "GTAPTrade", subtype = paste(gtap_version, "VXMD", sep = "_"),
                      bilateral = bilateral, aggregate = FALSE)
@@ -35,7 +34,7 @@ calcTradeMargin <- function(gtap_version = "GTAP7", bilateral = FALSE, producer_
   voa <- calcOutput(type = "GTAPTrade", subtype = paste(gtap_version, "VOA", sep = "_"),
                     bilateral = bilateral, aggregate = FALSE)
 
-  # difference imports exports/exports at mkt price * (value of output mkt prices/payment received at fgate)
+  # (imports-exports world price)/exports at mkt price * (value of output mkt prices/payment received at fgate)
   y <- setYears(vtwr / vxmd * (vom / voa), NULL)
 
   y[is.infinite(y)] <- NA
@@ -51,11 +50,14 @@ calcTradeMargin <- function(gtap_version = "GTAP7", bilateral = FALSE, producer_
 
   y <- fillMean(y)
 
+ # NOTE all producer prices except for FAOini have products missing from FAOini. These will get 0 in MAgPIE
   if (is.null(producer_price)) {
     producer_price <- "FAOp" # nolint
   }
   if (producer_price %in% c("IMPACT3.2.2World_Price", "FAO", "FAOp", "WBGEM")) {
     p <- collapseNames(calcOutput("PriceAgriculture", datasource = producer_price, aggregate = FALSE))[, 2005, ]
+  } else if (producer_price == "FAOini") {
+   p <- calcOutput("IniFoodPrice", products = "k_trade", aggregate = FALSE)
   } else {
 (stop("Valid food price is required"))
 }
@@ -73,32 +75,36 @@ if (bilateral) {
  }
   out <- y[, , kTrade] * p[, , kTrade]
 
-
  if (bilateral) {
   out <- toolCountryFillBilateral(out, 0)
  } else {
 out <- toolCountryFill(out, 0)
 }
 
-
 ##### make countries with 0 margins high
-# take max by product and exporting region
+# take max by product and (exporting) region
+mapping <- toolGetMapping("regionmappingH12.csv", type = "regional")
+reg <- unique(mapping$RegionCode)
+
 for (i in getNames(out)) {
-  for (t in getItems(out, dim = 1.1)) {
-tmp <- out[list("Regions" = t), , i]
-tmp[which(tmp == 0)]  <- max(tmp)
-out[list("Regions" = t), , i] <- tmp
+  for (r in reg) {
+tmp <- out[list("Regions" = mapping[which(mapping$RegionCode == r), "CountryCode"]), , i]
+# round to include values that were in range of e-14
+tmp[which(round(tmp, 3) == 0)]  <- max(tmp)
+out[list("Regions" =  mapping[which(mapping$RegionCode == r), "CountryCode"]), , i] <- tmp
 }
 }
 # take 99 percetile by product for if still 0
-for (i in getNames(out)) {
+for (i in where(out == 0)$true$data) {
 tmp <- out[, , i]
-tmp[which(tmp == 0)]  <- quantile(tmp, .99)
+tmp[which(tmp == 0)]  <- quantile(tmp, .75)
 out[, , i] <- tmp
 }
 
+ if (!any(c("wood", "woodfuel") %in% getNames(weight))) {
   out <- add_columns(x = out, addnm = findset("kforestry"), dim = 3.1)
   out[, , findset("kforestry")] <- out[, , "others"]
+ }
 
   weight <- setYears(vxmd * voa, NULL)[, , kTrade]
   if (bilateral) {
@@ -107,14 +113,26 @@ out[, , i] <- tmp
     weight <- toolCountryFill(weight, 0)
 }
 
+  if (!any(c("wood", "woodfuel") %in% getNames(weight))) {
   weight <- add_columns(x = weight, addnm = findset("kforestry"), dim = 3.1)
 
   weight[, , "wood"] <- weight[, , "tece"] * 0.5
   weight[, , "woodfuel"] <- weight[, , "wood"] * 0.5
+  }
 
-
-
-
+  # fill in data in ktrade but not in GTAP
+  out <- add_columns(x = out, addnm =  setdiff(findset("k_trade"), getNames(out)), dim = 3.1)
+  weight <- add_columns(x = weight, addnm =  setdiff(findset("k_trade"), getNames(weight)), dim = 3.1)
+  out[, , "brans"] <- out[, , "maiz"]
+  weight[, , "brans"] <- weight[, , "maiz"]
+  out[, , "ethanol"] <- out[, , "oils"]
+  weight[, , "ethanol"] <- weight[, , "oils"]
+  out[, , "scp"] <- out[, , "sugar"]
+  weight[, , "scp"] <- weight[, , "sugar"]
+  out[, , "betr"] <- out[, , "wood"]
+  weight[, , "betr"] <- weight[, , "wood"]
+  out[, , "begr"] <- out[, , "sugr_cane"]
+  weight[, , "begr"] <- weight[, , "sugr_cane"]
 
   unit <- "US$05"
   description <- "Trade margins"
