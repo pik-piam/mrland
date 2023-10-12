1c29f4e9c73c7a7be44723e07639457e3172b12a
Debbora Leip <leip@pik-potsdam.de>
2023-10-12 14:48:34 +0200

    fixed toolGetMapping warnings in some convert functions

--- R/convertREMIND.R
+++ R/convertREMIND.R
@@ -1,22 +1,22 @@
-convertREMIND <- function(x,subtype) {
-  
+convertREMIND <- function(x, subtype) {
+
   # remove global dimension
-  x <- x["GLO",,,invert=TRUE]
-  
-  map <- toolGetMapping(type = "regional", name = "regionmappingH12.csv")
-  
-  if(grepl("intensive",subtype)) {
+  x <- x["GLO", , , invert = TRUE]
+
+  map <- toolGetMapping(type = "regional", name = "regionmappingH12.csv", where = "madrat")
+
+  if (grepl("intensive", subtype)) {
     # No weight for disaggregation because it's prices
-    y <- toolAggregate(x,map) 
-    
-  } else if(grepl("extensive",subtype)) {
+    y <- toolAggregate(x, map)
+
+  } else if (grepl("extensive", subtype)) {
     # Use population of 2010 as weight for disaggregation
-    pop <- calcOutput("Population",aggregate=FALSE)
-    y <- toolAggregate(x,map,weight=pop[,2010,1])
-    
+    pop <- calcOutput("Population", aggregate = FALSE)
+    y <- toolAggregate(x, map, weight = pop[, 2010, 1])
+
   } else {
-    stop("Unknown subtype ",subtype)
+    stop("Unknown subtype ", subtype)
   }
-  
+
   return(y)
 }
