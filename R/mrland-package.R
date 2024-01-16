7cf0b23b990c1a0512fac4f24d124377a74d4ada
Pascal Führlich <82826417+pfuehrlich-pik@users.noreply.github.com>
2024-01-16 10:21:54 +0100

    Update mrland-package.R

--- R/mrland-package.R
+++ R/mrland-package.R
@@ -1,13 +1,9 @@
-
-
 #' MadRaT land data package
 #' 
 #' The package provides land related data via the madrat framework.
 #' 
-#' 
 #' @name mrland-package
 #' @aliases mrland-package mrland
-#' @docType package
 #' @author 
 #' Maintainer: Jan Philipp Dietrich <dietrich@pik-potsdam.de>
-NULL
+"_PACKAGE"
