1d984bfc86c8739a7adb86a99d8a652145a5f23d
caviddhen <david.chen@pik.potsdam.de>
2025-08-07 10:51:16 +0200

    update UrbanLandFuture

--- R/calcUrbanLandFuture.R
+++ R/calcUrbanLandFuture.R
@@ -10,29 +10,30 @@
 #' @importFrom mstools toolHoldConstant
 #' @importFrom magclass nregions setCells getCells
 
-calcUrbanLandFuture <- function(timestep = "5year", subtype = "LUH2v2",
+calcUrbanLandFuture <- function(timestep = "5year", subtype = "LUH3",
                                 cells = "lpjcell", cellular = TRUE) {
 
-  if (subtype == "LUH2v2") {
+  if (subtype == "LUH3") {
     past <- calcOutput("LanduseInitialisation",
-      cellular = TRUE, nclasses = "seven", cells = "lpjcell",
-      selectyears = "past", input_magpie = FALSE, aggregate = FALSE
+      cellular = TRUE, nclasses = "seven",
+      selectyears = "past_til2020", input_magpie = FALSE, aggregate = FALSE
     )
-    past <- past[, c("y1995", "y2000", "y2005", "y2010"), "urban"]
+    past <- past[, c("y1995", "y2000", "y2005", "y2010", "y2015", "y2020"), "urban"]
     past <- add_columns(past, addnm = c("SSP2", "SSP3", "SSP4", "SSP5"), dim = 3.1)
     past[, , 2:5] <- past[, , 1]
     getNames(past)[1] <- "SSP1"
 
     out <- readSource("LUH2UrbanFuture", convert = "onlycorrect")
+    out <- out[, c(2015:2020), invert = TRUE]
 
     if (timestep == "5year") {
-      out <- out[, paste0("y", seq(2015, 2100, 5)), ]
+      out <- out[, paste0("y", seq(2025, 2100, 5)), ]
       out <- toolHoldConstant(out, paste0("y", seq(2105, 2150, 5)))
       out <- mbind(setCells(past, getCells(out)), out)
       names(dimnames(out)) <- c("x.y.iso", "t", "data")
     } else if (timestep == "yearly") {
       out <- toolHoldConstant(out, paste0("y", c(2101:2150)))
-      past <- time_interpolate(past, interpolated_year = 1995:2010)
+      past <- time_interpolate(past, interpolated_year = 1995:2020)
       out <- mbind(setCells(past, getCells(out)), out)
       names(dimnames(out)) <- c("x.y.iso", "t", "data")
     }
