bed64ad04d8a2f2ab6185840255078c1f9183559
caviddhen <david.chen@pik.potsdam.de>
2025-08-28 11:27:25 +0200

    indenting

--- R/calcProcessing_balanceflow.R
+++ R/calcProcessing_balanceflow.R
@@ -18,24 +18,24 @@
 
 calcProcessing_balanceflow <- function() { # nolint: object_name_linter.
   massbalance <- calcOutput("FAOmassbalance", aggregate = FALSE)
-
+  
   # add cotton fibres, cause its not in the massabalance calculations
   conv <- calcOutput("Processing_conversion_factors", aggregate = FALSE)[, , "ginning"]
   massbalance[, , "fibres.production_estimated.dm"] <-
     collapseNames(conv[, getYears(massbalance), "ginning.fibres.cottn_pro"] *
                     massbalance[, , "cottn_pro.production.dm"])
-
+  
   ksd <- findset("ksd")
   ksdNoscp <- setdiff(ksd, "scp")
   balanceflow <- dimSums(massbalance[, , "production"][, , "dm"][, , ksdNoscp], dim = c(3.2, 3.3)) -
     dimSums(massbalance[, , "production_estimated"][, , "dm"][, , ksdNoscp], dim = c(3.2, 3.3))
   balanceflow <- add_columns(balanceflow, addnm = "scp", dim = 3.1)
   balanceflow[, , "scp"] <- 0
-
+  
   out <- toolHoldConstantBeyondEnd(balanceflow)
   # fading out the balanceflow until 2050.
   out <- convergence(origin = out, aim = 0, start_year = "y2015", end_year = "y2050", type = "s")
-
+  
   return(list(x = out, weight = NULL, unit = "t DM",
               description = paste0("Balanceflow to balance out the assumption of uniform ",
                                    "conversion factors worldwide but match FAO.")))
--- R/calcProcessing_conversion_factors.R
+++ R/calcProcessing_conversion_factors.R
@@ -15,64 +15,64 @@
 
 calcProcessing_conversion_factors <- function() { # nolint: object_name_linter.
   massbalance <- calcOutput("FAOmassbalance", aggregate = FALSE)
-
+  
   kpr <- findset("kpr")
   ksd <- findset("ksd")
   kprocessing <- findset("processing20")
   mbReduced <- dimSums(massbalance[, , "dm"], dim = c(1, 3.3))
   kmb <- paste("X", kpr, sep = "")
-
+  
   productionEstimated <- dimSums(mbReduced[, , "production_estimated"][, , ksd], dim = 3.2)
   convmatrix <- add_dimension(x = productionEstimated, dim = 3.2, add = "kpr", nm = kmb)
   convmatrix <- add_dimension(x = convmatrix, dim = 3.1, add = "processing", nm = kprocessing)
   convmatrix[, , ] <- 0
-
+  
   mbReduced2 <- mbReduced[, , kpr]
-
+  
   tmp <- dimSums(mbReduced2[, , c("alcohol1", "alcohol2", "alcohol3", "alcohol4")], dim = 3.2) /
     dimSums(mbReduced2[, , "fermentation"], dim = 3.2)
   convmatrix[, , "alcohol"][, , "fermentation"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "brewers_grain1"], dim = 3.2) /
     dimSums(mbReduced2[, , "fermentation"], dim = 3.2)
   convmatrix[, , "distillers_grain"][, , "fermentation"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "brans1"], dim = 3.2) /
     dimSums(mbReduced2[, , "milling"], dim = 3.2)
   convmatrix[, , "brans"][, , "milling"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "branoil1"], dim = 3.2) /
     dimSums(mbReduced2[, , "milling"], dim = 3.2)
   convmatrix[, , "oils"][, , "milling"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "oilcakes1"], dim = 3.2) /
     dimSums(mbReduced2[, , "milling"], dim = 3.2)
   convmatrix[, , "oilcakes"][, , "milling"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "distillers_grain1"], dim = 3.2) /
     dimSums(mbReduced2[, , "distilling"], dim = 3.2)
   convmatrix[, , "distillers_grain"][, , "distilling"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "ethanol1"], dim = 3.2) /
     dimSums(mbReduced2[, , "distilling"], dim = 3.2)
   convmatrix[, , "ethanol"][, , "distilling"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "molasses1"], dim = 3.2) /
     dimSums(mbReduced2[, , "refining"], dim = 3.2)
   convmatrix[, , "molasses"][, , "refining"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , c("sugar1", "sugar2", "sugar3")], dim = 3.2) /
     dimSums(mbReduced2[, , "refining"], dim = 3.2)
   convmatrix[, , "sugar"][, , "refining"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , "oilcakes1"], dim = 3.2) /
     dimSums(mbReduced2[, , "extracting"], dim = 3.2)
   convmatrix[, , "oilcakes"][, , "extracting"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   tmp <- dimSums(mbReduced2[, , c("oil1", "oil2")], dim = 3.2) /
     dimSums(mbReduced2[, , "extracting"], dim = 3.2)
   convmatrix[, , "oils"][, , "extracting"] <- setNames(tmp, paste0("X", getNames(tmp)))
-
+  
   # add conversion attributes of Single cell Protein (SCP)
   # based on Table S3 in Pikaar et al 2018, which provides conversion factors
   # as "ton substrate DM / ton microbial protein (MP)"
@@ -85,7 +85,7 @@ calcProcessing_conversion_factors <- function() { # nolint: object_name_linter.
   convmatrix[, , "breeding"][, , "scp"][, , "Xbegr"] <- 1 / 5.5 # 0.1818
   convmatrix[, , "breeding"][, , "scp"][, , "Xsugr_cane"] <- 1 / 4.3 # 0.2326
   convmatrix[, , "breeding"][, , "scp"][, , "Xfoddr"] <- 1 / 5.6 # 0.1786
-
+  
   tmp <- collapseNames(dimSums(massbalance[, , "production"][, , "dm"][, , "fibres"], dim = 1) /
                          dimSums(massbalance[, , "production"][, , "dm"][, , "cottn_pro"], dim = 1))
   convmatrix[, , "ginning"][, , "fibres"][, getYears(tmp), "Xcottn_pro"] <- tmp
@@ -93,27 +93,27 @@ calcProcessing_conversion_factors <- function() { # nolint: object_name_linter.
   convmatrix[is.na(convmatrix)] <- 0
   convmatrix[is.infinite(convmatrix)] <- 0
   # add conversion attributes of bioenergy crops into ethanol
-
+  
   convmatrix[, , "extracting"][, , "ethanol"][, , "Xbetr"] <- 0.36
   convmatrix[, , "extracting"][, , "ethanol"][, , "Xbegr"] <- 0.36
-
-    # test that the conversion factors are congruent with the 
-    # production_estimated in FAOmassbalance_pre
-    kprocessingM <- setdiff(kprocessing, c("breeding", "ginning"))
-    mbReduced <- dimSums(massbalance[, , "dm"], dim = 3.3)
-
-    a <- mbReduced[, , kprocessingM][, , kpr]
-    getNames(a, dim = 1) <- paste0("X", getNames(a, dim = 1))
-    b <- dimSums(a * convmatrix[, , kprocessingM], dim = c(3.1, 3.2))
-    c <- dimSums(mbReduced[, , ksd][, , "production_estimated"], dim = 3.2)
-    if (any(round(b - c, 4) != 0)) {
-      stop("conversion factors are incoherent with production_estimated column of massbalance!")
-    }
-
+  
+  # test that the conversion factors are congruent with the 
+  # production_estimated in FAOmassbalance_pre
+  kprocessingM <- setdiff(kprocessing, c("breeding", "ginning"))
+  mbReduced <- dimSums(massbalance[, , "dm"], dim = 3.3)
+  
+  a <- mbReduced[, , kprocessingM][, , kpr]
+  getNames(a, dim = 1) <- paste0("X", getNames(a, dim = 1))
+  b <- dimSums(a * convmatrix[, , kprocessingM], dim = c(3.1, 3.2))
+  c <- dimSums(mbReduced[, , ksd][, , "production_estimated"], dim = 3.2)
+  if (any(round(b - c, 4) != 0)) {
+    stop("conversion factors are incoherent with production_estimated column of massbalance!")
+  }
+  
   getNames(convmatrix, dim = 3) <- substring(getNames(convmatrix, dim = 3), 2)
-
+  
   convmatrix <- toolHoldConstantBeyondEnd(convmatrix)
-
+  
   return(list(x = convmatrix,
               weight = NULL,
               unit = "t DM / t DM",
