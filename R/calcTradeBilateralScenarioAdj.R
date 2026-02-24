b85fb7a3edb20e0220215d1d47bcddfa8c1b8cdf
caviddhen <david.chen@pik.potsdam.de>
2026-02-24 11:52:32 +0100

    bugfix years trade

--- R/calcTradeBilateralScenarioAdj.R
+++ R/calcTradeBilateralScenarioAdj.R
@@ -3,13 +3,20 @@
 #' @description Creates dummy trade scenario adjustments file filld with 0's,
 #' actual changes currently implemented in the model on the import supply ratio
 #' (imports/domestic supply)
+#' @param magYears logical or character vector. If TRUE, filters to t_all 5-year
+#' time steps using findset("t_all").
 #' @return dummy file with 0's to befilled in MAgPIE
 #' @author David M Chen
 #'
-calcTradeBilateralScenarioAdj <- function() {
+calcTradeBilateralScenarioAdj <- function(magYears = TRUE) {
 
   tm <- calcOutput("TradeBilateralFAOHarmonized", aggregate = FALSE, yearly = TRUE)
   tm[] <- 0
+
+  if (magYears) {
+    tAll <- findset("t_all")
+    tm <- tm[, intersect(getYears(tm), tAll), ]
+  }
   tm <- toolHoldConstantBeyondEnd(tm)
 
   return(list(x = tm,
