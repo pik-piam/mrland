4d1616e83eb2719295431ef62833295a577d1c0e
AnkitSaha07 <119042828+AnkitSaha07@users.noreply.github.com>
2026-09-09 13:40:51 +0200

    Update R/calcEATLancetTargets.R
    Co-authored-by: Felicitas Beier <39262100+FelicitasBeier@users.noreply.github.com>

--- R/calcEATLancetTargets.R
+++ R/calcEATLancetTargets.R
@@ -39,7 +39,7 @@ calcEATLancetTargets <- function(attributes = "kcal/d") {
 
   relMatrixFood <- cbind(eatFood, eatTFood15)
 
-  #changed dim to 3.2 bcz 3.1 is dietscen now
+  # dim to 3.2 is the dietscen dimension
   eat <- toolAggregate(eatTargets, rel = relMatrixFood,
                        dim = 3.2, partrel = TRUE,
                        from = "eatFood", to = "eatTFood15")
